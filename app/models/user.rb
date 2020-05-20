99a39927e75461bf30795f98a1c6bc33ec0c12d7
nhannn-2212 <nguyen.ngoc.nhan@sun-asterisk.com>
2020-05-20 15:15:38 +0700

    chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # constant
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  # attr macro
+
+  # relation macro
+
+  # validation macro
+
+  validates :email, presence: true, length: {maximum: 55},
+    uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :password, presence: true, length: {minimum: 6}
+  # callback macro
+  before_save{email.downcase!}
+
+  # scopes
+
+  has_secure_password
+end
